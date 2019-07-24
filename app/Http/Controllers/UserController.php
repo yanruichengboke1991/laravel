a51a1ceef92c46bb5fa041c476611d7d4da0a166
aidong <aidong@aidongdeMac-mini.local>
2019-07-24 15:35:59 +0800

    提交测试

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+
+  function index(){
+  	echo 111;die();
+  }
+
+
+
+
+
+};
