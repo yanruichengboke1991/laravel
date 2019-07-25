f0e4112347d5ea17fc145d85c65e90f45d2e534a
aidong <aidong@aidongdeMac-mini.local>
2019-07-25 15:28:07 +0800

    各种查询语句

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,21 +8,31 @@ class UserController extends Controller
 {
 
 
-  function index(){
-  	$msg['status'] = 'success';
-  	$msg['errorcode'] = 200;
-  	$msg['msg'] = 'ok';
-  	echo json_encode($msg);
-  }
+    function index(){
+    	$msg['status'] = 'success';
+    	$msg['errorcode'] = 200;
+    	$msg['msg'] = 'ok';
+    	echo json_encode($msg);
+    }
 
 
+    function test2(){
+      	$card = DB::select("select goods_id,cat_id,goods_name from goods");
+        //去除满足条件的第一条
+        $ca = DB::table("users")->where('address_id',">","1")->first();
+        //取出满足所有条件的内容
+        $cd = DB::table("users")->where('address_id','>',"1")->get();
+        //返回满足条件的user_name字段的内容
+        $cf = DB::table("users")->where("address_id",">","1")->pluck("user_name");
+        //返回一个组合值"232059cb5361a9336ccf1b8c2ba7657a" => "vip3"
+        $ch = DB::table("users")->where("address_id",">","1")->pluck("user_name",'password');
+        // 指定查询自居 查询用户名和密码
+        $cj = DB::table("users")->select("user_name","password")->get();
+        dd($cj);
+    }
 
-
-  function test(){
-
-	$student = DB::select("select * from card");
-	dd($student);
-
-  }
-
+    function show(){
+      $id = 1;
+      return view('User.profile',['User'=>User::findOrfail($id)]);
+    }
 };
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,22 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+| contains the "web" middleware group. Now create something great! 
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//配置两个测试路由
+//配置两个测试路由   
 Route::get('/user','Usercontroller@index');
-Route::get('/user/test','Usercontroller@test');
+
+
+Route::get('/user/test2','Usercontroller@test2');
+
+
+
+// Route::get('foo',function(){
+// 	return 'hello world!';
+// });
+
